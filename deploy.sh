f02acbd7c642d2748967d8d5e34844ca63752205
Jamie Hewland <jhewland@gmail.com>
2017-06-19 16:52:48 +0200

    deploy.sh: Use tabs

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,15 @@ set -e
 
 self="$(basename "$0")"
 usage() {
-  cat <<-EOU
-usage: $self TAG_IN TAG_OUT...
-   ie: $self myimage myimage:latest myimage:0.3.2
-EOU
+	cat <<-EOU
+		usage: $self TAG_IN TAG_OUT...
+		   ie: $self myimage myimage:latest myimage:0.3.2
+	EOU
 }
 eusage() {
-  echo >&2 "error: $1"
-  usage >&2
-  exit 1
+	echo >&2 "error: $1"
+	usage >&2
+	exit 1
 }
 
 TAG_IN="$1"; shift || eusage 'missing TAG_IN'
